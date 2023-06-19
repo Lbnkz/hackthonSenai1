1c55776e7188ae7cb68e5fbbcf6b793e85e2305f
Lbnkz <lucasbaixonunes@gmail.com>
2023-06-19 15:56:06 -0300

    style update, function repare

--- src/js/script.js
+++ src/js/script.js
@@ -1,54 +1,4 @@
-/*
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDFuVNR5zBhehcrHOQCsaPD4urbCcOmuRA",
-  authDomain: "cantina-sesi-senaiu.firebaseapp.com",
-  projectId: "cantina-sesi-senaiu",
-  storageBucket: "cantina-sesi-senaiu.appspot.com",
-  messagingSenderId: "32917445635",
-  appId: "1:32917445635:web:5962f08940201e0e9f0236",
-  measurementId: "G-67RNKTP0T7"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-//Cadastra Pedido
-function cadastraPedido(item, valor, quantidade) {
-  console.log("Foi")
-  // Acesso ao Firestore
-  var db = firebase.firestore();
-
-  // Coleção onde os pedidos serão armazenados
-  var pedidosCollection = db.collection('pedidos');
-
-  // Definir os dados do pedido
-  var novoPedido = {
-    item: item,
-    valor: valor,
-    quantidade: quantidade,
-    data: new Date()
-  };
-
-  // Adicionar o novo pedido ao Firestore
-  pedidosCollection.add(novoPedido)
-    .then(function(docRef) {
-      // O pedido foi enviado com sucesso
-      console.log('Pedido enviado com sucesso! ID do documento:', docRef.id);
-    })
-    .catch(function(error) {
-      // Ocorreu um erro ao enviar o pedido
-      console.error('Erro ao enviar o pedido:', error);
-    });
-}
-*/
 
 //////////////////////////////
 
@@ -62,23 +12,28 @@ function verificaLista(itemb){
     b++
   }
 }
-function addItem(item, ide) {
-  var qnt = document.getElementById(ide).value
-  var existe = verificaLista(item)
-if(qnt != 0){
-  if(typeof existe === 'number'){
-    num = obj[existe].Quantidade
-    obj[existe].Quantidade = parseInt(num) + parseInt(qnt)
+function addItem(item, ide, id) {
+  var qnt = document.getElementById(ide).value;
+  var vlr = document.getElementById(id).textContent;
+  vlr = parseFloat(vlr.replace("R$ ", "").replace(",", "").replace("00", ""));
+
+  var existe = verificaLista(item);
+
+  if (qnt != 0) {
+    if (typeof existe === 'number') {
+      num = obj[existe].Quantidade;
+      obj[existe].Quantidade = parseInt(num) + parseInt(qnt);
+      obj[existe].Valor = vlr * obj[existe].Quantidade;
+    } else {
+      obj.push({ Produto: item, Quantidade: parseInt(qnt), Valor: vlr * parseInt(qnt) });
+    }
   }
-  else{
-  obj.push({ Produto: item, Quantidade: parseInt(qnt) });
-}
-}
-console.log(obj)
+
+  return obj;
 }
 
-function criarComanda(){
-  for(i in obj){
-    alert(obj[i])
-  }
+
+function exibirObjeto() {
+  alert(JSON.stringify(obj));
 }
+
